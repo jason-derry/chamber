5dd3ac5984779661917e58957f367ce3307f6085
jasond-qa <jason.derry@academytrainee.com>
2019-01-22 17:29:03 +0000

    register

---
+++ src/Register.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import Logo from './Logo';
+import './Register.css';
+import axios from 'axios';
+import { BrowserRouter as Link } from "react-router-dom";
+
+class Register extends Component {
+    constructor() {
+        super();
+        this.state = {
+          username: "",
+          password: "",
+          email: "",
+        };
+      }
+
+      handleUsernameChange = (event) => {
+         this.setState({username: event.target.value});
+      }
+      handleEmailChange = (event) => {
+        this.setState({email: event.target.value});
+     }
+     handlePasswordChange = (event) => {
+        this.setState({password: event.target.value});
+     }
+        
+    handleInput = () => {
+        axios({
+            method: "post",
+            url: "http://localhost:8081/chamber-api/api/chamber/createAccount",
+            data: {
+                username: this.state.username,
+                password: this.state.password,
+                email: this.state.email
+            }
+        });
+      }
+
+    render() {
+        return (
+            <div>
+                <div className="logLogo"><Logo /></div>
+                <div className="register">
+                    <Form>
+                        <FormGroup inline>
+                            <Label for="username" hidden>username</Label>
+                            <Input type="username" name="username" id="username" placeholder="username" value={this.state.username} onChange={this.handleUsernameChange} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="email" hidden>email</Label>
+                            <Input type="email" name="email" id="email" placeholder="email" value={this.state.email} onChange={this.handleEmailChange} />
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="password" hidden>password</Label>
+                            <Input type="password" name="password" id="password" placeholder="password" value={this.state.password} onChange={this.handlePasswordChange} />
+                        </FormGroup>
+                        <Button className="createAccButton" onClick={this.handleInput}>Create account</Button>
+                    </Form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Register;
--- src/Routing.js
+++ src/Routing.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Login from './Login';
 import Navigation from './Navigation';
 import WeaponList from './WeaponList';
+import Register from './Register';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class Routing extends Component {
@@ -12,6 +13,7 @@ class Routing extends Component {
           <Route exact path="/" component={Login} />
           <Route exact path="/nav" component={Navigation} />
           <Route path="/weapons" component={WeaponList} />
+          <Route path="/register" component={Register} />
           </div>
       </Router>
     );
